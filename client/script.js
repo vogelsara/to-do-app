19c80e6475cdf1f33a7f6389edf4ea4fe19ac016
Sari <sari.vogel.94@gmail.com>
2020-06-21 20:47:55 +0200

    Save input data to server

--- client/script.js
+++ client/script.js
@@ -11,3 +11,28 @@ function start() {
     xhttp.open("GET", "/api/todo", true);
     xhttp.send();
   }
+
+function createTodos(event){
+    event.preventDefault();
+    var title = document.getElementById("titleInput").value;
+    var date = document.getElementById("dateInput").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      var response = JSON.stringify(this.responseText);
+      console.log(response);
+      if (this.readyState == 4 && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+      }
+    }
+    xhttp.open("POST", "/api/todo", true);
+    var body = {
+      title: title,
+      date: date
+    };
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(body));
+    console.log(body.title + body.date);
+    
+  }
