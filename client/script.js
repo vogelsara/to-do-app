acaaa03c057fae7ec54d90d954ac3c409373a8a0
Sari <sari.vogel.94@gmail.com>
2020-06-22 00:24:48 +0200

    Render the saved todos

--- client/script.js
+++ client/script.js
@@ -4,8 +4,10 @@ function start() {
   makeRequest("GET", "/api/todo", [], function(responseText) {
     var todos = JSON.parse(responseText);
     console.log(todos);
+    renderTodos(todos);
   });
-  }
+}
+
 
 function createTodos(event){
     event.preventDefault();
@@ -22,7 +24,28 @@ function createTodos(event){
     var row = document.createElement('p');
     showTodoContainer.appendChild(row);
     row.innerHTML = body.title + " " + body.date;
+
   }
+
+
+
+  function renderTodos(todo){
+    var showTodoContainer = document.getElementById('showTodo');
+    
+    for(var i = 0; i < todo.length; i++){
+      var listElement = document.createElement("li");
+      var content = todo[i];
+      var checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+
+      listElement.innerHTML = content.title + " " + content.date;
+      showTodoContainer.appendChild(listElement);
+      listElement.appendChild(checkbox);
+    }
+  }
+
+
+
 
   function makeRequest(method, url, body, onResponse){
     var xhttp = new XMLHttpRequest();
