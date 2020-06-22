4f4dbe20e8a42f7ad664c083b5cdcc1acdedf511
Sari <sari.vogel.94@gmail.com>
2020-06-22 22:17:57 +0200

    Clean up

--- server.js
+++ server.js
@@ -8,38 +8,36 @@ app.use(express.static('./client'));
 app.get('/api/todo', (req, res) => res.json(todos));
 
 app.post('/api/todo', (req, res) => {
-    if(!req.body.title){
+    if (!req.body.title) {
         res.status(400).json({message: "Title is missing :("});
     }
-    else if(!req.body.date){
+    else if (!req.body.date) {
         res.status(400).json({message: "Date is missing"});
-    }else{
+    } else {
         const todo = {id: idIndex++, ...req.body};
         todos.push(todo);
         res.status(201).json(todo);
     }
-
 });
 
 app.delete('/api/todo/', (req, res) => {
     var todosToDelete = req.body;
     
-    for(var i = 0; i < todosToDelete.length; i++){
-        for(var j = 0; j < todos.length; j++){
-            if(todos[j]["id"] == parseInt(todosToDelete[i])){
+    for (var i = 0; i < todosToDelete.length; i++) {
+        for (var j = 0; j < todos.length; j++) {
+            if (todos[j]["id"] == parseInt(todosToDelete[i])) {
                 todos.splice(j, 1);
             }
         }
     }
     res.status(200).json();
-
 });
 
-app.use((req, res) =>{
-    res.status(404)
+app.use((req, res) => {
+    res.status(404);
     res.json({
         message: "Could not find the resource."
-    })
+    });
 })
 
 app.listen(3000, () => console.log('server is listening'));
